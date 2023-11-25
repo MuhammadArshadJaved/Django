fd4c63b30da01806f89c1a592105b3f6142560de
muhammadarshadjaved <iarshadjaved04@gmail.com>
2023-11-25 16:38:20 +0500

    adding multiple pages

--- project/hello/urls.py
+++ project/hello/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('brian', views.brian, name="brian"),
+    path('ali', views.ali, name="ali"),
 ]
--- project/hello/views.py
+++ project/hello/views.py
@@ -5,3 +5,9 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Hello, World")
+
+def brian(request):
+    return HttpResponse("Hello Brian")
+
+def ali(request):
+    return HttpResponse("Hello Ali")
