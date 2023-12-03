a6fd56ee9501885562afdea58da1c7847d7eb441
muhammadarshadjaved <iarshadjaved04@gmail.com>
2023-12-03 08:00:28 +0500

    task form created

--- project/tasks/views.py
+++ project/tasks/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from django import forms
+from django.urls import reverse
 # Create your views here.
 
-task=['foo','baz','buz']
+tasks=['foo','baz','buz']
+
+class newtaskform(forms.Form):
+    task= forms.CharField(label='new task')
 
 def index(request):
     return render(request, 'tasks/index.html',{
-        'tasks': task
+        'tasks': tasks
     })
 
 def add(request):
-    return render(request, 'tasks/add.html')
+    if request.method =='POST':
+        form=newtaskform(request.POST)
+        if form.is_valid():
+            task=form.cleaned_data['task']
+            tasks.append(task)
+            return HttpResponseRedirect(reverse('tasks:index'))
+        else:
+            return render(request, 'task/add.html',{
+                'form':form
+            })
+    return render(request, 'tasks/add.html',{
+        'form':newtaskform()
+    })
